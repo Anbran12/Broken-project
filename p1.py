edf24e78d877e80ecfaf8239c53fc9c96cac3ed8
Yeison Andres Bran Henao <yeison.bran.h@multienlace.com.co>
2025-02-18 19:00:48 -0500

    prueba

--- p1.py
+++ p1.py
@@ -1,5 +1,23 @@
-import metodos as m
+class MetodosMatrices:
+    def CrearMatriz(self):
+        dimensiones = int(input("Ingresa las dimensiones de la matriz: "))
+        self.dimensiones = dimensiones
+        Matriz = [[0 for _ in range(self.dimensiones)]for _ in range (self.dimensiones)]
+        self.Matriz = Matriz
 
-def punto1(self):
-    Matriz = m.crearMatriz(3,3)
-    m.mostrarMatriz(Matriz)
+    def MostrarMatriz(self):
+        for i in len(self.Matriz):
+            for j in len(self.Matriz):
+                return self.Matriz[i][j]
+
+    def LlenarMatriz(self):
+        for i in len(self.Matriz):
+            for j in len(self.Matriz):
+                dato = input("Ingresa un dato: ")
+                self.Matriz[i][j] = dato
+
+
+Obj_prueba = MetodosMatrices()
+Obj_prueba = CrearMatriz()
+Obj_prueba = Mostrarmatriz()
+Obj_prueba = LlenarMatriz()
